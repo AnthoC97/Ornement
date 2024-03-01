5f156142acb35da512abb11d6a7f50571ce914b0
AnthoC97 <anthony_contrevilliers@yahoo.fr>
2024-03-01 14:03:59 +0100

    Adding several parameters in text-decoration style attribute

--- Ornement.js
+++ Ornement.js
@@ -1,31 +1,36 @@
 const Ornement = (text) => {
   return text.replace(/^\[T(\{font-style:"([buis]+)"\})\] (.*$)/gim, (_, style, fontStyles, content) => {
     let styleAttribute = '';
+    let textDecoration = '';
 
-    for (const char of fontStyles) {
-      switch (char) {
-        case 'b':
-          styleAttribute += 'font-weight: bold;';
-          break;
-        case 'i':
-          styleAttribute += 'font-style: italic;';
-          break;
-        case 's':
-          styleAttribute += 'text-decoration: line-through;';
-          break;
-        case 'u':
-          styleAttribute += 'text-decoration: underline;';
-          break;
-        // Add more cases if needed for other styles
+    if (fontStyles.includes('b')) {
+      styleAttribute += 'font-weight: bold;';
+    }
+    if (fontStyles.includes('i')) {
+      styleAttribute += 'font-style: italic;';
+    }
+    if (fontStyles.includes('s')) {
+      if (textDecoration === '') {
+          textDecoration = 'text-decoration: line-through {};';
+      } else {
+        textDecoration = textDecoration.replace(" {}", " line-through {}");
+      }
+    }
+    if (fontStyles.includes('u')) {
+      if (textDecoration === '') {
+          textDecoration = 'text-decoration: underline {};';
+      } else {
+        textDecoration = textDecoration.replace(" {}", " underline {}");
       }
     }
 
-    return `<h1 style="${styleAttribute}">${content}</h1>`;
+    textDecoration = textDecoration.replace(" {}", "");
+    return `<h1 style="${styleAttribute}${textDecoration.trim()}">${content}</h1>`;
   });
 };
 
 const coolMessage = `
-[T{font-style:"ui"}] To all of my friends:
+[T{font-style:"sui"}] To all of my friends:
 **Live long and prosper!!!**
 `;
 
