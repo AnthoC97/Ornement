d9cc59855fa91c0dec059f9ee8456a402929a220
AnthoC97 <anthony_contrevilliers@yahoo.fr>
2025-07-16 01:13:04 +0200

    Adding default but insufficient xss protection

--- Ornement.js
+++ Ornement.js
@@ -3,8 +3,13 @@ import { media_tag_regex, generateHtmlMediaFromLine } from "./Parsers/MediaParse
 import { list_tag_regex, generateHtmlListFromLine } from "./Parsers/ListParser.js";
 import { code_tag_regex, generateHtmlCodeFromLine } from "./Parsers/CodeParser.js";
 
-function Ornement(text, targetId){
+
+
+const anti_xss_regex = /<.*>.*<.*>/g;
+
+function Ornement(text){
   const htmlTags = text
+	.replace(anti_xss_regex, "")
     .replace(list_tag_regex, (allLine, content) => {
       return generateHtmlListFromLine(allLine, content);
     })
@@ -19,28 +24,5 @@ function Ornement(text, targetId){
     });
     return htmlTags;
 };
-
-const coolMessage = `
-[T{font-style:"sui", link:"https://www.google.com/", color:"#FF0000"}] To all of my friends:[/T]
-[T{link:"https://www.google.com/",  color:"#00FF00", font-style:"sui" }] To all of my friends:[/T]
-[T{link:"https://www.google.com/",  highlight-color:"#00FF00"}] To all of my friends:[/T]
-To all of my [T{style:"sup",  highlight-color:"#00FF00", font-style:"i"}]friends:[/T]
-[M{src:"test.jpg"}]Test[/M]
-[M{src:"test.mkv"}]Test[/M]
-[M{src:"test.ogg"}]Test[/M]
-[L{type:"n"}]
-# tetst;
-# test[T{style:"sub",  highlight-color:"#00FF00", font-style:"i"}]2[/T];
-# test3;
-# [M{src:"test.ogg"}]Test[/M];
-[/L]
-[C]
-# tetst;
-  # test 2;
-# test3;
-[/C]
-`;
-
-console.log(Ornement(coolMessage));
 
 export { Ornement }
